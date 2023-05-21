1fd49a80d32b6dbe4d87470a95246c6f7d4a7ff1
54D4 <sada2005@wp.pl>
2023-05-21 03:07:20 +0200

    Update Todos.jsx

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const Todos = ({ name, id, deleteFunction }) => {
   return (
     <div
-      className="flex justify-between w-[364px] px-4 py-2 bg-gradient-to-r from-purple-400 to-pink-600 text-white  text-lg rounded-xl my-7 shadow-md"
+      className="flex flex-wrap  justify-between w-[364px]  px-4 py-2 bg-gradient-to-r from-purple-400 to-pink-600 text-white  text-lg rounded-xl my-7 shadow-md"
       id={id}
     >
       <h1>{name}</h1>
